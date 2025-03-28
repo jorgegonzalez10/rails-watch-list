fc0f37991c8f3c835b687951184cf06a4471b39f
jorge gonzalez <gonzalezjor13@gmail.com>
2025-03-28 10:21:25 -0500

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +8,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :lists, only: %i[index new create show] do
+    resources :bookmarks, only: :create
+  end
+  resources :bookmarks, only: :destroy
 end
